79053a3aeb3b9acbf5e8c222235847faa654732c
Henrique91 <henriquesilvacorrea91@gmail.com>
2022-09-11 12:15:43 -0300

    criado teste para git clesses

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testegit.Classes
+{
+    public class Pessoa
+    {
+        
+    }
+}
